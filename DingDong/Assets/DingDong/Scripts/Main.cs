208ae7aa5a0f06f34de549b7616a82d891977e29
Leon <lowpolygonum@gmail.com>
2015-10-25 19:27:31 +0100

    Refactoring unity project with separated experiments

---
+++ DingDong/Assets/DingDong/Scripts/Main.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using System.Collections;
+
+namespace DingDong
+{
+	public class Main : MonoBehaviour
+	{
+		public bool cycleMode = false;
+
+		Shader shader;
+		RenderTexture renderTexture;
+		Video video;
+		Sound sound;
+
+		void Start ()
+		{
+			shader = GetComponent<Shader>();
+			renderTexture = GetComponentInChildren<RenderTexture>();
+			video = GetComponentInChildren<Video>();
+			sound = GetComponent<Sound>();
+			// sound.LoadAudioClip();
+		}
+
+		void Resize (float pixelSize)
+		{
+			if (renderTexture.PixelSize != pixelSize)
+			{
+				renderTexture.PixelSize = pixelSize;
+				renderTexture.Resize();
+			}
+		}
+
+		void Update ()
+		{
+			UnityEngine.Shader.SetGlobalFloat("_TimeElapsed", Time.time);
+			UnityEngine.Shader.SetGlobalFloat("_MouseX", Input.mousePosition.x / Screen.width);
+			UnityEngine.Shader.SetGlobalFloat("_MouseY", Input.mousePosition.y / Screen.height);
+
+
+	        if (video.movieTexture.isReadyToPlay && !video.movieTexture.isPlaying)
+	        {
+	            video.movieTexture.Play();
+				// sound.SetAudioClip(video.movieTexture.audioClip);
+	        }
+
+	        if (video.movieTexture.isPlaying)
+	        {
+	        	sound.UpdateSamples();
+	        }
+
+			if (cycleMode)
+			{
+				if (shader.IsItTimeToChange())
+				{
+					shader.NextShader();
+					CheckSpecials();
+				}
+			}
+
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				shader.NextShader();
+				// CheckSpecials();
+			}
+
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				shader.PreviousShader();
+				// CheckSpecials();
+			}
+
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				// video.Toggle();
+			}
+
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				Application.Quit();
+			}
+		}
+
+		void CheckSpecials ()
+		{
+			if (shader.GetCurrentShader().name == "Custom/GlitchColorDirection"
+				|| shader.GetCurrentShader().name == "Custom/GlitchFatPixel"
+				|| shader.GetCurrentShader().name == "Custom/GlitchDistortion2"
+				|| shader.GetCurrentShader().name == "Custom/GlitchRain")
+			{
+				Resize(4f);
+			}
+			else
+			{
+				Resize(1f);
+			}
+		}
+	}
+}
---
+++ DingDong/Assets/DingDong/Scripts/Sound.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.Collections;
+
+namespace DingDong
+{
+	public class Sound : MonoBehaviour
+	{
+		AudioClip audioClip;
+		AudioSource audioSource;
+    	float[] samples;
+    	Texture2D samplesTexture;
+    	float samplesTotal;
+    	float samplesElapsed;
+
+		void Start ()
+		{
+	        foreach (string device in Microphone.devices) {
+	            Debug.Log("Name: " + device);
+	        }
+
+			samplesElapsed = 0f;
+			const int length = 256;
+			samples = new float[length];
+			samplesTexture = new Texture2D(length, 1, TextureFormat.ARGB32, false);
+			samplesTotal = 0f;
+			UnityEngine.Shader.SetGlobalTexture("_SamplerSound", samplesTexture);
+			audioSource = gameObject.AddComponent<AudioSource>();
+			
+			audioClip = Microphone.Start(Microphone.devices[0], true, 5, 24000);
+			audioSource.clip = Microphone.Start(Microphone.devices[0], true, 5, 24000);
+
+			    // audioObj[index].clip = ;
+
+
+		}
+
+	    public void UpdateSamples ()
+	    {
+	        if (audioClip.loadState == AudioDataLoadState.Loaded && !audioSource.isPlaying)
+	        {
+	            audioSource.Play();
+	        }
+
+	        audioSource.GetSpectrumData(samples, 0, FFTWindow.BlackmanHarris);
+
+	        Color[] colors = samplesTexture.GetPixels(0);
+	        samplesTotal = 0f;
+	        for (int i = 0; i < samples.Length; ++i)
+	        {
+	        	float sample = samples[i];
+
+	        	// colors[i] = Color.white * sample;
+	        	// samplesTotal += sample;
+
+	        	if (sample > 0.0001f)
+	        	{
+	        		colors[i] = Color.white;
+	        		samplesTotal += 1f;
+	        	}
+	        	else
+	        	{
+	        		colors[i] = Color.black;
+	        	}
+	        }
+			samplesTexture.SetPixels(colors, 0);
+	        samplesTexture.Apply(false);
+
+	        samplesElapsed += samplesTotal;
+
+			UnityEngine.Shader.SetGlobalFloat("_SamplesTotal", samplesTotal / (float)samples.Length);
+
+			UnityEngine.Shader.SetGlobalFloat("_SamplesElapsed", samplesElapsed);
+	    }
+	}
+}
