07afe668ab17b1bf5e64dc65778b72738e1868d9
Leon Denise <lowpolygonum@gmail.com>
2015-12-19 14:33:32 +0100

    webcam script + tweaking

---
+++ DingDong/Assets/DingDong/Scripts/Render/WebcamTexture.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamTexture : MonoBehaviour {
+
+	WebCamTexture textureWebcam;
+	public Material material;
+
+	void Start () {
+
+		if (WebCamTexture.devices.Length > 0) {
+			// Setup webcam texture
+			textureWebcam = new WebCamTexture();
+			Shader.SetGlobalTexture("_WebcamTexture", textureWebcam);
+			textureWebcam.Play();
+			if (material != null) {
+				material.mainTexture = textureWebcam;
+			}
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
