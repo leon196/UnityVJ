948f03922139e77ef29fb72f7d8370fb8f68f250
Leon <lowpolygonum@gmail.com>
2016-01-21 01:34:53 +0100

    Clean integration

--- Assets/Gossima/quads/wireframe.js
+++
@@ -1,7 +0,0 @@
-﻿// Attach this script to a camera, this will make it render in wireframe
-	function OnPreRender() {
-		GL.wireframe = true;
-	}
-	function OnPostRender() {
-		GL.wireframe = false;
-	}
